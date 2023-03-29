5e079a6f0b671cd933e8c0f8c79e97b73889f751
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-03-29 13:09:34 -0400

    Revert "whalen fixed"
    This reverts commit 5b754200d275dc8b0284300ef69bebe6f5e80fbd.

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
 
 const app = createApp(App)
@@ -10,4 +9,4 @@ app.use(router)
 
 app.mount('#app')
 
-console.log("test")
+// function createCard = 
