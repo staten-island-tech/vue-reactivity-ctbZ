da3ff7ff7216727ec654914351afa3599a35d78c
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-03-29 20:58:30 -0400

    sd
    asdasdasd

---
+++ vue/src/App.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">test</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+}
+</style>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+      <h1 v-if="graduated">{{ student }}</h1>
+      <h1 v-else>They did not yet graduate</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home"
+  components: {
+    student: String,
+    graduated: Boolean,
+    animals: String
+  },
+  data() {
+      return {
+          student: "Harry",
+          graduated: true,
+          animals: [""]
+      
+      };
+  },
+};
+</script>
