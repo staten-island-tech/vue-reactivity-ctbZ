7ece3f44aa4b59f2c71e933790ac80975c96a64b
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-04-10 13:24:27 -0400

    restart

--- vue/src/views/Array.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="home">
-        <h1 v-if="graduated">{{ student }}</h1>
-        <h1 v-else>They did not yet graduate</h1>
-    </div>
-</template>
--- vue/src/views/test.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="test">
-      <h1>Testing 101</h1>
-    </div>
-  </template>
-  
